9208a898a0a7e50527865750bc8f4dab6c2e87ad
Aryan-cyber-guy <flyhigh7674@gmail.com>
2026-03-01 11:22:40 +0530

    some other changes

--- database.py
+++ database.py
@@ -2,10 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv(
-    "postgresql://product_db_p5ew_user:wdzQAkaoGaocbFn3Koc5pRI6LSk6530H@dpg-d6ht1eogjchc73cumf80-a/product_db_p5ew",
-    "postgresql://postgres:password@localhost:5432/mydb"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(
     DATABASE_URL,
     connect_args={"sslmode": "require"}  # Required for Render
